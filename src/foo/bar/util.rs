ad6e096b5f19931a587acbee7e9eeb2a8271fd7a
kengo-k <31839234+kengo-k@users.noreply.github.com>
2023-07-10 18:37:05 +0900

    サブディレクトリのモジュールやインラインのモジュールを使用する形に修正

---
+++ src/foo/bar/util.rs
@@ -0,0 +1,5 @@
+use super::super::common::MyEnum;
+
+pub fn print_my_enum(e: &MyEnum) {
+    println!("enum value: {:?}", e);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,40 @@
+mod foo;
+
+mod inner_mod {
+    pub fn hello() {
+        println!("Hello");
+    }
+
+    pub mod inner_inner_mod {
+        pub fn world() {
+            println!("World");
+        }
+    }
+
+    #[derive(Debug)]
+    pub struct Person {
+        pub name: String,
+    }
+
+    impl Person {
+        pub fn new(name: String) -> Self {
+            Self { name }
+        }
+
+        pub fn show(&self) {
+            println!("{:?}", self);
+        }
+    }
+}
+
+use crate::foo::common::MyEnum;
+use inner_mod::inner_inner_mod::world;
+use inner_mod::Person;
+
 fn main() {
-	println!("Hello, world!");
+    inner_mod::hello();
+    world();
+    let john = Person::new("John".to_string());
+    john.show();
+    foo::bar::util::print_my_enum(&MyEnum::A);
 }
